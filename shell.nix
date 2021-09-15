ae97a3568e7fb10f66b44d2c53c7a1e5014b78b9
Silvan Mosberger <contact@infinisil.com>
2021-09-15 13:21:27 +0200

    More reproducible with cabal and stack

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/071317d543205ee5f5611d391a37582f9b282240.tar.gz";
+    sha256 = "00s7lnwijrwssnn8ggmd66yqvix7j2p9w7my6ksyynfgf68yl7z0";
+  };
+  pkgs = import nixpkgs {
+    overlays = [];
+    config = {};
+  };
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs.haskellPackages; [
+    ghc
+    cabal-install
+  ];
+}
