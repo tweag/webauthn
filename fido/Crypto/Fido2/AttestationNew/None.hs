81fba3e99d233c22b3b415ac61cf3b3274d47f6f
Silvan Mosberger <contact@infinisil.com>
2021-10-19 16:46:49 +0200

    Rename None to AttestationStatementFormatNone and export the format type
    Needed to implement Arbitrary instances for tests

--- fido/Crypto/Fido2/AttestationNew/None.hs
+++ fido/Crypto/Fido2/AttestationNew/None.hs
@@ -1,26 +1,26 @@
 {-# LANGUAGE TypeFamilies #-}
 
-module Crypto.Fido2.AttestationNew.None (asfNone) where
+module Crypto.Fido2.AttestationNew.None (asfNone, AttestationStatementFormatNone (AttestationStatementFormatNone)) where
 
 import qualified Crypto.Fido2.AttestationNew as V
 import qualified Crypto.Fido2.Model as M
 import qualified Crypto.Fido2.Model.JavaScript.Decoding as D
 import Data.Void (Void)
 
-data None = None
+data AttestationStatementFormatNone = AttestationStatementFormatNone
   deriving (Show)
 
-instance M.AttestationStatementFormat None where
-  type AttStmt None = ()
+instance M.AttestationStatementFormat AttestationStatementFormatNone where
+  type AttStmt AttestationStatementFormatNone = ()
   asfIdentifier _ = "none"
 
-instance D.DecodableAttestationStatementFormat None where
-  type AttStmtDecodingError None = Void
+instance D.DecodableAttestationStatementFormat AttestationStatementFormatNone where
+  type AttStmtDecodingError AttestationStatementFormatNone = Void
   asfDecode _ _ = Right ()
 
-instance V.VerifiableAttestationStatementFormat None where
-  type AttStmtVerificationError None = Void
+instance V.VerifiableAttestationStatementFormat AttestationStatementFormatNone where
+  type AttStmtVerificationError AttestationStatementFormatNone = Void
   asfVerify _ _ _ _ = Right M.AttestationTypeNone
 
 asfNone :: D.SomeAttestationStatementFormat
-asfNone = D.SomeAttestationStatementFormat None
+asfNone = D.SomeAttestationStatementFormat AttestationStatementFormatNone
