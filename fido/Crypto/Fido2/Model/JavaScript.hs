db66f49b0d140981845e13d44353f84f3b8c59ab
Silvan Mosberger <contact@infinisil.com>
2021-10-19 13:52:47 +0200

    Add better documentation and TODO's to DOMString and USVString

--- fido/Crypto/Fido2/Model/JavaScript.hs
+++ fido/Crypto/Fido2/Model/JavaScript.hs
@@ -68,13 +68,24 @@ import Deriving.Aeson
   )
 import GHC.Generics (Generic)
 
--- TODO: It's probably not okay for both DOMString and USVString to be the
--- same type, figure out how they need to differ
-
 -- | [(spec)](https://heycam.github.io/webidl/#idl-DOMString)
+-- The `[DOMString](https://webidl.spec.whatwg.org/#idl-DOMString)` type
+-- corresponds to the set of all possible sequences of
+-- [code units](https://webidl.spec.whatwg.org/#dfn-code-unit). Such sequences
+-- are commonly interpreted as UTF-16 encoded strings
+-- [RFC2781](https://webidl.spec.whatwg.org/#biblio-rfc2781) although this is not required.
+-- TODO: This implementation doesn't allow invalid UTF-16 codepoints, which
+-- probably makes it not work regarding https://www.w3.org/TR/webauthn-2/#sctn-strings
+-- Write a test case that doesn't work and find a better representation
 type DOMString = Text
 
 -- | [(spec)](https://heycam.github.io/webidl/#idl-USVString)
+-- The `[USVString](https://webidl.spec.whatwg.org/#idl-USVString)` type
+-- corresponds to the set of all possible sequences of
+-- [Unicode scalar values](http://www.unicode.org/glossary/#unicode_scalar_value),
+-- which are all of the Unicode code points apart from the surrogate code points.
+-- TODO: This implementation allows for surrogate code points. Figure out if
+-- this can violate the spec in any way.
 type USVString = Text
 
 -- | [(spec)](https://heycam.github.io/webidl/#idl-unsigned-long)
