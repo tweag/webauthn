cdb5096d0b96838070f34f208671b6ac6602932e
Silvan Mosberger <contact@infinisil.com>
2022-01-05 20:58:27 +0100

    Single-line values

--- src/Module.hs
+++ src/Module.hs
@@ -8,38 +8,7 @@ import Data.Coerce (coerce)
 import GHC.Generics
 import Data.Aeson
 
-data SomeType
-  = SomeValue0
-  | SomeValue1
-  | SomeValue2
-  | SomeValue3
-  | SomeValue4
-  | SomeValue5
-  | SomeValue6
-  | SomeValue7
-  | SomeValue8
-  | SomeValue9
-  | SomeValue10
-  | SomeValue11
-  | SomeValue12
-  | SomeValue13
-  | SomeValue14
-  | SomeValue15
-  | SomeValue16
-  | SomeValue17
-  | SomeValue18
-  | SomeValue19
-  | SomeValue20
-  | SomeValue21
-  | SomeValue22
-  | SomeValue23
-  | SomeValue24
-  | SomeValue25
-  | SomeValue26
-  | SomeValue27
-  | SomeValue28
-  | SomeValue29
-  deriving Generic
+data SomeType = SomeValue0 | SomeValue1 | SomeValue2 | SomeValue3 | SomeValue4 | SomeValue5 | SomeValue6 | SomeValue7 | SomeValue8 | SomeValue9 | SomeValue10 | SomeValue11 | SomeValue12 | SomeValue13 | SomeValue14 | SomeValue15 | SomeValue16 | SomeValue17 | SomeValue18 | SomeValue19 | SomeValue20 | SomeValue21 | SomeValue22 | SomeValue23 | SomeValue24 | SomeValue25 | SomeValue26 | SomeValue27 | SomeValue28 | SomeValue29 deriving Generic
   deriving FromJSON via CustomJSON
 
 newtype CustomJSON = CustomJSON SomeType
