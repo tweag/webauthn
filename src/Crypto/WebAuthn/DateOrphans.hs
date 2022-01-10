0a844e3e9930d60b3f27fa75b3ad87d0d21fbce0
Silvan Mosberger <contact@infinisil.com>
2022-01-10 13:29:30 +0100

    Add Timeable (Fixed a) date orphan
    The Data.Fixed module contains an alias for `Milli` which
    is useful for date conversion with the android safetynet attestation
    statement format, which has an integer field representing milliseconds.

--- src/Crypto/WebAuthn/DateOrphans.hs
+++ src/Crypto/WebAuthn/DateOrphans.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 
--- | This module contains orphan instances connecting three different date libraries together:
+-- | This module contains orphan instances connecting different date libraries together:
 --
 -- * [time](https://hackage.haskell.org/package/time), a commonly used library
 --   containing the 'UTCTime' type, which is a bit slow and inconvenient to use
@@ -10,6 +10,9 @@
 -- * [hourglass](https://hackage.haskell.org/package/hourglass), an alternative
 --   to the time library which is nicer to use. It is used by the
 --   [x509-validation](https://hackage.haskell.org/package/x509-validation) library
+-- * [Data.Fixed](https://hackage.haskell.org/package/base/docs/Data-Fixed.html)
+--   in `base`, which is used as the underlying representation of `NominalDiffTime`
+--   in the `time` library.
 --
 -- This module contains a 'Timeable' and 'Time' implementation for 'UTCTime',
 -- and a 'MonadTime' implementation for any 'ReaderT' of a 'Timeable'
@@ -17,20 +20,26 @@ module Crypto.WebAuthn.DateOrphans () where
 
 import Control.Monad.Reader (ReaderT, asks)
 import Control.Monad.Time (MonadTime, currentTime)
-import Data.Fixed (Fixed (MkFixed))
+import Data.Fixed (Fixed (MkFixed), HasResolution, Nano)
 import Data.Hourglass (Elapsed (Elapsed), ElapsedP (ElapsedP), NanoSeconds (NanoSeconds), Seconds (Seconds), Time, Timeable, timeConvert, timeFromElapsedP, timeGetElapsedP)
 import Data.Time (UTCTime, nominalDiffTimeToSeconds, secondsToNominalDiffTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime, utcTimeToPOSIXSeconds)
 
 instance Timeable UTCTime where
-  timeGetElapsedP utcTime = ElapsedP seconds nanos
-    where
-      (s, MkFixed n) = properFraction $ nominalDiffTimeToSeconds $ utcTimeToPOSIXSeconds utcTime
-      seconds = Elapsed $ Seconds s
-      nanos = NanoSeconds $ fromInteger n
+  timeGetElapsedP utcTime =
+    timeGetElapsedP $ nominalDiffTimeToSeconds $ utcTimeToPOSIXSeconds utcTime
 
 instance Time UTCTime where
   timeFromElapsedP = posixSecondsToUTCTime . secondsToNominalDiffTime . realToFrac
+
+instance HasResolution a => Timeable (Fixed a) where
+  timeGetElapsedP value = ElapsedP seconds nanos
+    where
+      ns :: Nano
+      ns = realToFrac value
+      (s, MkFixed n) = properFraction ns
+      seconds = Elapsed $ Seconds s
+      nanos = NanoSeconds $ fromInteger n
 
 instance (Timeable t, Monad m) => MonadTime (ReaderT t m) where
   currentTime = asks timeConvert
