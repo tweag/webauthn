673a5bdfd4a84b407af1164dbfd0315a813c2397
Sumit Raja <sumitraja@gmail.com>
2023-03-08 11:46:04 +1100

    Allow MDS blob parsing to handle invalid entries
    Enable MDS blob parsing to handle invalid entries without completely failing to parse

--- tests/MetadataSpec.hs
+++ tests/MetadataSpec.hs
@@ -3,6 +3,7 @@
 
 module MetadataSpec (spec) where
 
+import Crypto.WebAuthn.Metadata (metadataBlobToRegistry)
 import Crypto.WebAuthn.Metadata.Service.Processing (RootCertificate (RootCertificate), fidoAllianceRootCertificate, jsonToPayload, jwtToJson)
 import Crypto.WebAuthn.Metadata.Service.WebIDL (MetadataBLOBPayload, entries, legalHeader, nextUpdate, no)
 import Data.Aeson (Result (Success), ToJSON (toJSON), decodeFileStrict, fromJSON)
@@ -13,6 +14,7 @@ import Data.HashMap.Strict ((!), (!?))
 import qualified Data.PEM as PEM
 import qualified Data.Text as Text
 import Data.Text.Encoding (decodeUtf8)
+import Data.These (These (That, These, This))
 import qualified Data.X509 as X509
 import qualified Data.X509.CertificateStore as X509
 import Spec.Util (predeterminedDateTime)
@@ -49,8 +51,9 @@ golden subdir = describe subdir $ do
   it "can decode and reencode the payload to the partially parsed JSON" $ do
     Just value <- decodeFileStrict $ "tests/golden-metadata/" <> subdir <> "/payload.json"
     case jsonToPayload value of
-      Left err -> fail $ show err
-      Right _result -> pure ()
+      This err -> fail $ show err
+      These err _result -> fail $ show err
+      That _result -> pure ()
 
 spec :: SpecWith ()
 spec = do
@@ -61,3 +64,10 @@ spec = do
     it "can validate the payload" $ do
       blobBytes <- BS.readFile "tests/golden-metadata/big/blob.jwt"
       jwtToJson blobBytes fidoAllianceRootCertificate predeterminedDateTime `shouldSatisfy` isRight
+  describe "MDS with errors" $ do
+    it "can process an MDS file with errors" $ do
+      blobBytes <- BS.readFile "tests/golden-metadata/big/blob-with-errors.jwt"
+      case metadataBlobToRegistry blobBytes predeterminedDateTime of
+        Right (These _errs _res) -> pure ()
+        Right _thisThat -> error "Expected parsing errors as well as registry"
+        Left err -> error $ Text.unpack err
