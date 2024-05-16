5ce902ab081ea4eefa55916a7e20f9bcc2fe6e1d
Sjoerd Visscher <sjoerd.visscher@tweag.io>
2024-05-16 16:42:04 +0200

    Update metadata tests

--- tests/MetadataSpec.hs
+++ tests/MetadataSpec.hs
@@ -4,21 +4,20 @@
 module MetadataSpec (spec) where
 
 import Crypto.WebAuthn.Metadata (metadataBlobToRegistry)
-import Crypto.WebAuthn.Metadata.Service.Processing (RootCertificate (RootCertificate), fidoAllianceRootCertificate, jsonToPayload, jwtToJson)
-import Crypto.WebAuthn.Metadata.Service.WebIDL (MetadataBLOBPayload, entries, legalHeader, nextUpdate, no)
+import Crypto.WebAuthn.Metadata.Service.Processing (RootCertificate (RootCertificate), ProcessingError, fidoAllianceRootCertificate, jwtToAdditionalData)
+import Crypto.WebAuthn.Metadata.Service.WebIDL (MetadataBLOBPayload)
 import Data.Aeson (Result (Success), ToJSON (toJSON), decodeFileStrict, fromJSON)
 import Data.Aeson.Types (Result (Error))
 import qualified Data.ByteString as BS
 import Data.Either (isRight)
-import Data.HashMap.Strict ((!), (!?))
 import qualified Data.PEM as PEM
 import qualified Data.Text as Text
 import Data.Text.Encoding (decodeUtf8)
-import Data.These (These (That, These, This))
+import Data.These (These (These))
 import qualified Data.X509 as X509
 import qualified Data.X509.CertificateStore as X509
 import Spec.Util (predeterminedDateTime)
-import Test.Hspec (SpecWith, describe, it, shouldBe, shouldSatisfy)
+import Test.Hspec (SpecWith, describe, it, shouldSatisfy)
 import Test.Hspec.Expectations.Json (shouldBeUnorderedJson)
 
 golden :: FilePath -> SpecWith ()
@@ -32,14 +31,11 @@ golden subdir = describe subdir $ do
         store = X509.makeCertificateStore [cert]
 
     blobBytes <- BS.readFile $ "tests/golden-metadata/" <> subdir <> "/blob.jwt"
-    let Right result = jwtToJson blobBytes (RootCertificate store origin) predeterminedDateTime
+    let Right result = jwtToAdditionalData blobBytes (RootCertificate store origin) predeterminedDateTime
 
     Just expectedPayload <- decodeFileStrict $ "tests/golden-metadata/" <> subdir <> "/payload.json"
 
-    (result !? "legalHeader") `shouldBe` toJSON <$> legalHeader expectedPayload
-    (result !? "no") `shouldBe` Just (toJSON (no expectedPayload))
-    (result !? "nextUpdate") `shouldBe` Just (toJSON (nextUpdate expectedPayload))
-    (result ! "entries") `shouldBeUnorderedJson` toJSON (entries expectedPayload)
+    toJSON (result :: MetadataBLOBPayload) `shouldBeUnorderedJson` expectedPayload
 
   it "can decode and reencode the payload to the partially parsed JSON" $ do
     Just payload <- decodeFileStrict $ "tests/golden-metadata/" <> subdir <> "/payload.json"
@@ -47,13 +43,6 @@ golden subdir = describe subdir $ do
       Error err -> fail err
       Success (value :: MetadataBLOBPayload) ->
         toJSON value `shouldBeUnorderedJson` payload
-
-  it "can decode and reencode the payload to the partially parsed JSON" $ do
-    Just value <- decodeFileStrict $ "tests/golden-metadata/" <> subdir <> "/payload.json"
-    case jsonToPayload value of
-      This err -> fail $ show err
-      These err _result -> fail $ show err
-      That _result -> pure ()
 
 spec :: SpecWith ()
 spec = do
@@ -63,7 +52,8 @@ spec = do
   describe "fidoAllianceRootCertificate" $ do
     it "can validate the payload" $ do
       blobBytes <- BS.readFile "tests/golden-metadata/big/blob.jwt"
-      jwtToJson blobBytes fidoAllianceRootCertificate predeterminedDateTime `shouldSatisfy` isRight
+      let metadata = jwtToAdditionalData blobBytes fidoAllianceRootCertificate predeterminedDateTime :: Either ProcessingError MetadataBLOBPayload
+      metadata `shouldSatisfy` isRight
   describe "MDS with errors" $ do
     it "can process an MDS file with errors" $ do
       blobBytes <- BS.readFile "tests/golden-metadata/big/blob-with-errors.jwt"
